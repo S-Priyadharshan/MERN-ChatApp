82c007215fc12845c20cafc32ed4f0341173163e
Visalan <visalanoffl@gmail.com>
2025-01-12 16:37:43 +0530

    welp

--- frontend/src/content/Home/Home.jsx
+++ frontend/src/content/Home/Home.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
 import './Home.css';
 
 import Navigation from '../../components/navigation/Navigation';
-import Sidebar from '../../components/sidebar/Sidebar'; 
+import Sidebar from '../../components/sidebar/Sidebar';
 import MessageContainer from '../../components/MessageContainer/MessageContainer';
 
 const Home = () => {
+
+
+  useEffect(() => {
+
+    const handleMove = (e) => {
+      const mousepos = { x: e.clientX, y: e.clientY };
+      curSpan.style.top = `${mousepos.y}px`;
+      curSpan.style.left = `${mousepos.x}px`;
+    }
+    
+    const curSpan = document.createElement('span');
+    curSpan.className = "cursor-blur";
+    document.body.appendChild(curSpan);
+    window.onmousemove = (e) => handleMove(e);
+
+    return () => {
+      window.onmousemove = null;
+      document.body.removeChild(curSpan);
+    };
+  }, []);
+
   return (
     <div className='app__home'>
-      <Navigation/>
-      <Sidebar/>
-      <MessageContainer/>
+      <Navigation />
+      <Sidebar />
+      <MessageContainer />
     </div>
   )
 }
